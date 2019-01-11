099b5ce0fafc0c19694c7e41f7e85bde443270c1
Lailson Conceição <lailsoncv@hotmail.com>
2019-01-11 14:51:05 -0200

    Adicionada função para lembrar a senha

--- php/login.php
+++ php/login.php
@@ -2,15 +2,19 @@
 
 require_once 'Crud.php';
 
-if($_POST && !empty($_POST)) {
+if($_POST && !empty($_POST) || isset($_COOKIE['usuario'])) {
     $usuario = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
     $senha   = md5(filter_var($_POST['password'], FILTER_SANITIZE_STRING));
+    $lembrar = isset($_POST['remember']);
+
+    if($lembrar)
+        setcookie('usuario', $usuario, time() + 86400);
 
     $crud = new Crud();
     $crud->setUsuario($usuario);
     $crud->setSenha($senha);
 
-    if($crud->hasLogin())
+    if($crud->hasLogin() || isset($_COOKIE['usuario']))
         echo 'Bem vindo à vida!';
     else
         echo 'Usuário ou senha incorreto. Ou seu cadastro ainda não foi confirmado.';
